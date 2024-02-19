4d1dda71162681c323016311965dd083bffe0566
claratoll <clara.toll@gmail.com>
2024-02-19 10:30:09 +0100

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,64 @@
-var display = true;
+const searchButton = document.getElementById("search_button");
+const searchField = document.getElementById("search_text");
+const imageField = document.getElementById("image");
+const repsField = document.getElementById("reps");
+const roundsField = document.getElementById("rounds");
+let reps = 12;
+let rounds = 4;
 
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+searchButton.addEventListener("click", async () => {
+  const searchTerm = searchField.value;
 
-console.log(date);
+  console.log(searchTerm);
 
-var date_of_pass = document.getElementById("2022-3-31").id;
+  if (searchTerm) {
+    const imagesData = await getImages(searchTerm);
+    updateUi(imagesData);
+  }
+});
 
-var anotherDay = document.getElementsByTagName("div");
-console.log(anotherDay);
+const updateUi = (data) => {
+  imageField.innerHTML = "";
+  if (data.length === 0) {
+    const noElement = document.createElement("p");
+    noElement.textContent = "No images found";
+    imageField.appendChild(noElement);
+  } else {
+    const exerciseNumber = Math.floor(Math.random() * data.length);
 
+    const exerciseImage = data[exerciseNumber];
 
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute("src", exerciseImage.source_url);
+    imgElement.setAttribute("alt", exerciseImage.title.rendered);
+    imageField.appendChild(imgElement);
+    randomWorkout();
+    repsField.innerHTML = reps + " repetitions";
+    roundsField.innerHTML = rounds + " rounds";
+  }
+};
 
-    if (date >= anotherDay) {
-        display = false;
+const getImages = async (searchTerm) => {
+  try {
+    const response = await fetch(
+      "https://strongandhappy.se/wp-json/wp/v2/media?search=" + searchTerm
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    } else {
+      console.log("it worked");
+      console.log(response);
     }
+    const imagesData = await response.json();
+    return imagesData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
 
-
-console.log(date_of_pass);
-
-console.log(display);
+const randomWorkout = () => {
+  reps = Math.floor(Math.random() * 15) + 1;
+  rounds = Math.floor(Math.random() * 4) + 1;
+};
